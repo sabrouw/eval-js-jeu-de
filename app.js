10ea3ebbc911794a3285000d3ada91d8173db6e2
sabrow <sabrow@hotmail.fr>
2022-04-06 16:54:50 +0200

    ajout js début jquery

--- app.js
+++ app.js
@@ -1,30 +1,94 @@
-import * as THREE from 'https://unpkg.com/three@0.139.0/build/three.module.js'
+//déclaration de variable utiles
+let hold = document.getElementById('hold');
+hold;
+let round = document.getElementById('round');
+round;
 
-// Création de la scène, de la caméra et du renderer
-let htmlCanvas = document.getElementById('canvas');
-let canvasElement = document.getElementById('canvas');
-let scene = new THREE.Scene();
+//bouton rolldice pour lancer le dé et donne un nombre aléatoire entre 1 et 6
+let rolldice = document.getElementById('roll');
+rolldice.onclick = function getRandomIntInclusive(min, max) {
+      min = Math.ceil(1);
+     max = Math.floor(6);     
+     console.log(Math.floor(Math.random() * (max - min +1)) + min);}
 
-let camera = new THREE.PerspectiveCamera(70, canvasElement.width / canvasElement.height, 0.1, 1000);
-    camera.position.z = 3;
 
 
-let renderer = new THREE.webGLRenderer({
-        canvas:htmlCanvas
-    });
+//variable de déclaration du score global et son appel
+let global = document.getElementById('global');
+global;
 
-    //creation de l'objet
-let geometry = new THREE.BoxGeometry(1, 2, 1);
-let box = new THREE.boxGeometry(geometry, material);
-let standard = new THREE.MeshStandardMaterial({
-    color: '#FF0000',
-    roughness: 0.304,
-});
-let boxMesh = new THREE.Mesh(geometry, basic);
-boxMesh.position.set(-2, 0, -3);
+//quand on clique sur hold ca ajouter rolldice au score global
+hold.onclick = function(){
+     console.log(global);
+      return global +++ rolldice;}
 
-//ajout de l'objet à la scene
-scene.add(box);
 
-//rendu de la scene
-renderer.render(scene, camera);
+;
+
+
+     //if (getRandomIntInclusive >= 2 && getRandomIntInclusive <= 6){
+     // global === true;
+     //}
+
+     
+     //let global;
+     //function global()
+     //{rolldice =+ getRandomIntInclusive
+
+     //}
+
+//si le player1 est = a true 
+//alors  rolldice = true
+
+$(document).ready(function(){
+     alert('jquery est bien chargé');
+}
+
+
+    
+
+
+
+
+    
+      
+    
+      
+      
+
+//nouvelle partie avec le player1 en premier
+//let newgame = document.getElementById('player1');
+//while 
+//
+//donne un nombre aléatoire de 1 à 6 pour le dé
+//le nombre aléatoire est attribué à la variable rolldice
+
+
+
+//round est réinitialisé si rounds = 1
+
+
+//if (rounds >= 2 && rounds <= 6 ) {
+//    console.log(rounds);}
+//else(rounds === 0);
+//
+////addition de tous les rounds obtenus et les envoyer dans le global
+//function additionRandom1(){
+//}
+
+//si le player1 joue alors le player 2 ne joue pas
+//if (player1 === true){getRandomIntInclusive();}
+//    else (player2 === true);
+
+
+
+
+
+
+
+
+
+
+
+
+
