8c2adc50e99b35311bba20772bbc1a09ac08475f
sabrow <sabrow@hotmail.fr>
2022-04-29 08:51:29 +0200

    changement de function de tableau aleatoire en random number

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const lancer1 = document.getElementById('roll1');
 const lancer2 = document.getElementById('roll2');
 const recup1 = document.getElementById('hold1');
 const recup2 = document.getElementById('hold2');
-let de;
 let global1;
 let global2;
 let round1 = [];
@@ -41,46 +40,14 @@ newGame.onclick = function resultats(){
   messageglobal2.textcontent = '0';
 };
 
-//*******************************TABLEAU DE 1 A 6************************************* 
-de = [1, 2, 3, 4, 5, 6];
-console.log('mon tableau de dé '+ de); 
 
-//**************************  FUNCTION QUI RANDOMIZE MON TABLEAU DE BASE************* */
-function randomize(de) {
-  var i, j, tmp;
-  for (i = de.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      tmp = de[i];
-      de[i] = de[j];
-      de[j] = tmp;
-  }
-  return de;
+//**************************  FUNCTION NOMBRE ALEATOIRE************* */
+function randomize(){
+  randomNumber = Math.floor(Math.random() * 6 ) + 1;
+  console.warn('valeur de dé' + randomNumber);
+    return randomNumber;
     }
-    randomize(de);
-    console.warn('tableau aléatoire' + de);
-    console.log('valeur de[1] après random:'+ de[1]);
- 
-    let de1 = de[1];
-    let de2 = de[2];
-    console.log('de1 :' + de1);
-    console.log('de2 :' + de2);
-/*********************************CONDITION DE VALEUR ENTRE 1 ET 6*********************** */
-function cond1() {
-  if (de1 >1 && de1< 7){
-    console.log('bravo');
-  }
-  else
-  activePlayer == player2;
-};
-
-function cond2() {
-  if (de2 >1 && de2< 7){
-    console.log('bravo');
-  }
-  else
-  activePlayer == player1;
-};
-
+      
 /*********************************FUNCTION ACTIVE PLAYER*********************** */
 function switchPlayer(){
   if (activePlayer === 'player1') {
@@ -89,10 +56,9 @@ function switchPlayer(){
   } else {
       activePlayer = 'player1';
       console.log('mon active player est : ' + activePlayer);
-  }
-};
+  }};
 
-//FONCTION QUI SAUVEGARDE LES POINTS DANS SCORE GLOBAL :
+/********************FONCTION QUI SAUVEGARDE LES POINTS DANS SCORE GLOBAL ************/
 function totalGlobal() {
   if (activePlayer === 'player1') {
       resultGlobal1 = resultRound1 += resultGlobal1;
@@ -102,7 +68,7 @@ function totalGlobal() {
       console.log(`le total du joueur 1 est : ${resultGlobal1} function globalPlayer`);
   } else if (activePlayer === 'player2') {
       resultGlobal2 = resultRound2 += resultGlobal2;
-      global2.textContent = resultGlobal2;
+      global2.innerHTML = resultGlobal2;
       resultRound2 = 0;
       round2.textContent = 0;
       console.log(`le total du joueur 2 est : ${resultGlobal2} function globalPlayer`);
@@ -110,7 +76,7 @@ function totalGlobal() {
 };
 /******************************CONDITION SI LE DE FAIT 1************************************ */
 function DiceOne(){
-  if (de1 === 1) {
+  if (randomNumber === 1) {
       if (activePlayer === 'player1') {
           activePlayer = 'player2';
           console.log(`le joueur passe la main au ${activePlayer} grâce à la fonction looseDiceOne `);
@@ -147,44 +113,21 @@ resultRound2 = round2.reduce(
          global2 = [resultRound2];
        console.log('affichage global2 :' + global2);
 };
-///*********************************FORMULE ROUND1 DANS GLOBAL1************************/
-//function totalGlobal1(){
-//  resultGlobal1 = global1.reduce(
-//    (resultGlobal1 , round1) => previousValue + currentValue)
-//    return resultGlobal1;
-//};
-//console.log("totalGlobal1 avec valeur dans parenthese: "+ resultGlobal1)
-//
-////************************FORMULE ROUND2 DANS GLOBAL2**********************************/
-//function totalGlobal2(){
-//  resultGlobal2 =  global2.reduce(
-//             (previousValue , currentValue) => global2 + round2)
-//             return resultGlobal2;
-//                               };
-
 
 //****************************APPEL DE MES FONCTIONS POUR BOUTON ROLLDICE1*****************
 lancer1.onclick = function resultats(){
-  randomize(de);
-  de[1];
-  console.log('function' + de[1])
-  resultImage1(de[1]);
-  //condition entre 1 et 6
-  cond1();
+  randomize()
+  console.log('nombre aleatoire' + randomNumber)
+  resultImage();
   //si je fait un je passe mon tour
   DiceOne();
   //push de[1] dans round1
-  round1.push(de[1]++);
-  //parcour du tableau de round1
-  for (k = 0; k <5; k++);
+  round1.push(randomNumber++);
   round1;
   totalRound1();
   //affiche la somme du tableau round1 sur ma page
   message1.innerHTML = resultRound1;
-  console.log('ajout des valeur dans round1:'+ round1);}
-       
-
-        //parcour de mon tableau round1
+  console.log('ajout des valeur dans round1:'+ round1);}        //parcour de mon tableau round1
         for(m = 0; m< round1.length; m++);
 
 
@@ -204,20 +147,14 @@ recup1.onclick = function (){
 
 //****************************APPEL DE MES FONCTIONS POUR BOUTON ROLLDICE2*****************
 lancer2.onclick = function resultats(){
-  randomize(de);
-  de[2];
-  resultImage2();
-   //condition entre 1 et 6
-  cond2();
+  randomize();
+  resultImage();
   //si je fait un je passe mon tour
   DiceOne();
   //push de[2] dans round2
-  round2.push(de[2]++);
-  //parcour du tableau de round2
-  for (l = 0; l <5; l++);
+  round2.push(randomNumber++);
   round2;
   totalRound2();
-  
   //affiche la somme du tableau round1 sur ma page
   message2.innerHTML = resultRound2;
   console.log('ajout des valeur dans round2:'+ round2);}
@@ -225,14 +162,12 @@ lancer2.onclick = function resultats(){
         //parcour de mon tableau round2
         for(n = 0; n< round2.length; n++);
 
-
 //****************************APPEL DE MES FONCTIONS POUR BOUTON HOLD1*****************   
 recup2.onclick = function (){
 //formule pour ajouter le resultRound2 au global2 à revoir
       totalGlobal();
-      switchPlayer();
-      
-    
+      console.log('totalglobal2'+totalGlobal)
+      switchPlayer();  
       //la valeur de global2 est maintenant resultGlobal2
       messageglobal2.innerHTML = resultGlobal2;
       message2.innerHTML = '0';
@@ -252,8 +187,8 @@ recup2.onclick = function (){
 
 
 /***************************FUNCTION POUR MES IMAGES DE VALEUR de[1]************************** */
-function resultImage1 () {
-  switch (randomize(de[1])){
+function resultImage () {
+  switch (randomize()){
   case 1: 
   let myImage1 = document.createElement('img');
   myImage1.src = 'https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312625_1280.png';
@@ -307,65 +242,15 @@ function resultImage1 () {
 };};
 
 
-  /***************************FUNCTION POUR MES IMAGES DE VALEUR de[2]************************** */
-  function resultImage2 () {
-    switch (randomize(de[2])){
-    case 1: 
-    let myImage1 = document.createElement('img');
-    myImage1.src = 'https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312625_1280.png';
-    document.getElementById('myImages').appendChild(myImage1).style.width= '100px';
-    
-                console.log(myImage1);
-    break;
-    case 2: 
-    let myImage2 = document.createElement('img');
-    myImage2.src = 'https://cdn.pixabay.com/photo/2014/04/03/10/24/two-310337__480.png';
-    document.getElementById('myImages').appendChild(myImage2).style.width = '100px';
-    
-          console.log(myImage2);  
-        break;
-        
-    case 3: 
-    let myImage3 = document.createElement('img');
-    myImage3.src = 'https://cdn.pixabay.com/photo/2014/04/03/10/24/three-310336__480.png';
-    document.getElementById('myImages').appendChild(myImage3).style.width = '100px';
-      
-       console.log(myImage3);
-       break;
-     
-    case 4: 
-    let myImage4 = document.createElement('img');
-    myImage4.src = 'https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312623__480.png';
-    document.getElementById('myImages').appendChild(myImage4).style.width = '100px';
-   
-       console.log(myImage4);
-       break;
-     
-    case 5: 
-    let myImage5 = document.createElement('img');
-    myImage5.src = 'https://cdn.pixabay.com/photo/2014/04/03/10/24/five-310334__480.png';
-    document.getElementById('myImages').appendChild(myImage5).style.width = '100px';
-    
-    
-        console.log(myImage5);
-        break;
-    case 6: let myImage6 = document.createElement('img');
-    myImage6.src = 'https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312621__480.png';
-    document.getElementById('myImages').appendChild(myImage6).style.width = '100px';
-    
-          console.log(myImage6);
-      break;
-      default:
-        console.log('mais que se passe-t-il');
-        break;
-  };};
+  
   //FONCTION QUI PERMET DE GRISER LE JOUEUR INACTIF:
   //function activePlayerStyle(){
   //  if (activePlayer === 'player1') {
   //    document.getElementsByClassName('roll2').style.color = black;
   //    document.getElementsByClassName('hold2').style.color = black;
   //  }
-  //    else{
+  //    else{ 
+   //       activePlayer === 'player';
   //      document.getElementsByClassName('roll1').style.color = black;
   //      document.getElementsByClassName('hold1').style.color = black;
   //    }};
