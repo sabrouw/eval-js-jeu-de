7400303c19068b6c74ff749776be4f8e1c6d676c
sabrow <sabrow@hotmail.fr>
2022-05-04 18:18:37 +0200

    changement de fonctionnalité de somme des round ok, mise au point du reset des score en cliquant sur new game

--- app.js
+++ app.js
@@ -16,155 +16,160 @@ const recup1 = document.getElementById('hold1');
 const recup2 = document.getElementById('hold2');
 let global1;
 let global2;
-let round1 = [];
-let round2 = [];
+let round1;
+let round2;
 let resultGlobal1 = 0;
 let resultGlobal2 = 0;
-let resultRound1;
-let resultRound2;
+let resultRound1 = 0;
+let resultRound2 = 0;
 let player1;
 let player2;
 let activePlayer;
 let newGame = document.getElementById('newgame');
-let randomNumber;
 let round = 0;
 
-console.log('active player est : '+ activePlayer)
-/*********************************BUTTON NEW GAME**********************************/
-newGame.onclick = function newGame(){
+
+/*********************************BUTTON NEW GAME RESET**********************************/
+newGame.onclick = function resultats(){
+  getRandomIntLancer()
   getRandomIntStart();
-  getRandomIntLancer();
-  start ();
-  console.log('mon active player est ${activePlayer}');
+  //supprime les images de dé
+  removeIn();
+  // mise a 0 scores
+  round1 = 0;
+  round2 = 0;
+  global1 = 0;
+  global2 = 0;
+  message1.innerHTML = '0';
+  message2.innerHTML = '0';
+  messageglobal1.innerHTML = '0';
+  messageglobal2.innerHTML= '0';
+  console.log('active player = ' + activePlayer);
 };
-// FONCTION POUR DEFINIR L'ACTIVE PLAYER :
+
+/***************************FUNCTIONS QUI DEFINISSENT L'ACTIVE PLAYER ********************************/
 function getRandomIntLancer() {
-  if (tour === 0) {
-      tour =  Math.floor(Math.random() * 2) + 1;
-      console.log(`function getRandomInt la valeur de tour est ${tour}`)
-      return activePlayer = `player${tour}`;
+  if (round === 0) {
+      round =  Math.floor(Math.random() * 2) + 1;
+      return activePlayer = `player${round}`;
       
-  }
-};
+  }};
 
 function getRandomIntStart() {
       tour =  Math.floor(Math.random() * 2) + 1;
       console.log(`function getRandomInt la valeur de tour est ${tour}`)
       return activePlayer = `player${tour}`;
-     
+      
 };
 
-function start (){
-  round1 = 0;
-  round2 = 0;
-  global1 = 0;
-  global2 = 0;
-  message1.textContent = '0';
-  message2.textcontent = '0';
-  messageglobal1.textcontent = '0';
-  messageglobal2.textcontent = '0';
-}
-
-console.log('mon active player est ${activePlayer}');
-
-//**************************  FUNCTION NOMBRE ALEATOIRE************* */
-function randomize(){
-  randomNumber = Math.floor(Math.random() * 6 ) + 1;
-  console.warn('valeur de randomumber :' + randomNumber);
-    return randomNumber;
-    };
-   
-/*********************************FUNCTION ACTIVE PLAYER*********************** */
+/*********************************FUNCTION ACTIVE PLAYER*******************************/
 function switchPlayer(){
   if (activePlayer === 'player1') {
-      activePlayer = 'player1';
+      lancer2.disabled = true;
+      activePlayer = 'player2';
       console.log(`function switchPlayer : ${activePlayer}`);
   } else {
-      activePlayer === 'player2';
+      activePlayer = 'player1';
+      lancer2.disabled = false;
       console.log('mon active player est : ' + activePlayer);
+  }};
+//**************************  FUNCTION NOMBRE ALEATOIRE************* ****************/
+function randomize(){
+  randomNumber = Math.floor(Math.random() * 6 ) + 1;
+  console.warn('valeur de dé' + randomNumber);
+    return randomNumber;
+    }
+      
+
+/********************FONCTION QUI SAUVEGARDE LES POINTS DANS SCORE GLOBAL ************/
+function totalGlobal() {
+  if (activePlayer === 'player1') {
+      resultGlobal1 = resultRound1 += resultGlobal1;
+      messageglobal1.textContent = resultGlobal1;
+      resultRound1 = 0;
+      message1.textContent = 0;
+      console.log(`le total du joueur 1 est : ${resultGlobal1} function globalPlayer`);
+  } else if (activePlayer === 'player2') {
+      resultGlobal2 = resultRound2 += resultGlobal2;
+      messageglobal2.innerHTML = resultGlobal2;
+      resultRound2 = 0;
+      message2.textContent = 0;
+      console.log(`le total du joueur 2 est : ${resultGlobal2} function globalPlayer`);
   }
 };
-console.log('mon active player est ${activePlayer}');
+
+/***************************FONCTION QUI STOCK LES POINTS DANS ROUND***********************/
+function totalRound(){
+  if (activePlayer === 'player1') {
+      resultRound1 = resultRound1 + randomNumber;
+      message1.innerHTML = resultRound1;
+      console.log(`function totaRound : le round1 est : ${resultRound1}`);
+      return resultRound1;
+  } else if (activePlayer === 'player2'){
+      resultRound2 = resultRound2 + randomNumber;
+      message2.innerHTML = resultRound2;
+      console.log(`function totaRound : le round2 est : ${resultRound2}`);
+      return resultRound2;
+  }
+};
+
 
 /******************************CONDITION SI LE DE FAIT 1************************************ */
 function DiceOne(){
   if (randomNumber === 1) {
       if (activePlayer === 'player1') {
-          lancer1.disabled = true;
-          lancer2.disabled = false;
-          message1.innerHTML = 0;
-          console.log(`le joueur passe la main au ${activePlayer} grâce à la fonction diceOne `);
+          activePlayer = 'player2';
+          console.log(`le joueur passe la main au ${activePlayer} grâce à la fonction DiceOne `);
           resultRound1 = 0;
-          }
-          
-     else if (activePlayer === 'player2'){
-      lancer2.disabled = true;
-      lancer2.disabled = false;
-      message2.innerHTML = 0;
-      resultRound2 = 0;
+          message1.innerHTML = resultRound1;
+          resultRound2 = 0;
+          message2.innerHTML= resultRound2;
+      } else {
+          activePlayer = 'player1';
+          console.log(`le joueur passe la main au ${activePlayer} grâce à la fonction DiceOne`);
+          resultRound2 = 0;
+          message2.innerHTML = resultRound2;
+          resultRound1 = 0;
+          message1.innerHTML = resultRound1;
       }
-      else{
-        console.log(activePlayer)
-      }};}
-console.log('mon active player est ${activePlayer}');
-
-/*********************************FORMULE AJOUTER SOMME ROUND1 DANS GLOBAL1************************/
-function totalRound1(){
-resultRound1 = round1.reduce(
-        (previousValue , currentValue) => previousValue + currentValue);
-        console.log('somme de mon tableau round1 :' +resultRound1);  
-               //j'ajoute la somme de mont tableau round1 dans ma variable global1
-       global1 = [resultRound1];
-       console.log('affichage global1 :' + global1);
-};
-
-/*********************************FORMULE AJOUTER SOMME ROUND1 DANS GLOBAL1************************/
-function totalRound2(){
-resultRound2 = round2.reduce(
-        (previousValue, currentValue) => previousValue + currentValue);
-        console.log('somme de mon tableau round2 :' +resultRound2);  
-        //j'ajoute la somme de mont tableau round2 dans ma variable global2
-         global2 = [resultRound2];
-       console.log('affichage global2 :' + global2);
-};
-/**************FONCTION QUI SAUVEGARDE LES POINTS DANS SCORE GLOBAL ******/
-function totalGlobal() {
-  if (activePlayer === 'player1') {
-      resultGlobal1 = resultRound1 += resultGlobal1;
-      global1.textContent = resultGlobal1;
-      resultRound1 = 0;
-      round1.textContent = 0;
-      console.log(`le total du joueur 1 est : ${resultGlobal1} function globalPlayer`);
-  } else if (activePlayer === 'player2') {
-      resultGlobal2 = resultRound2 += resultGlobal2;
-      global2.textContent = resultGlobal2;
-      resultRound2 = 0;
-      round2.textContent = 0;
-      console.log(`le total du joueur 2 est : ${resultGlobal2} function globalPlayer`);
   }
-};
-
+}
+/*********************************FORMULE AJOUTER SOMME ROUND1 DANS GLOBAL1************************/
+//function totalRound1(){
+//resultRound1 = round1.reduce(
+//        (previousValue , currentValue) => previousValue + currentValue);
+//        console.log('somme de mon tableau round1 :' +resultRound1);  
+//               //j'ajoute la somme de mont tableau round1 dans ma variable global1
+//       global1 = [resultRound1];
+//       console.log('affichage global1 :' + global1);
+//};
+//
+///*********************************FORMULE AJOUTER SOMME ROUND1 DANS GLOBAL1************************/
+//function totalRound2(){
+//resultRound2 = round2.reduce(
+//        (previousValue, currentValue) => previousValue + currentValue);
+//        console.log('somme de mon tableau round2 :' +resultRound2);  
+//        //j'ajoute la somme de mont tableau round2 dans ma variable global2
+//         global2 = [resultRound2];
+//       console.log('affichage global2 :' + global2);
+//};
+//
 //****************************APPEL DE MES FONCTIONS POUR BOUTON ROLLDICE1*****************
 lancer1.onclick = function resultats(){
-  console.log('mon activplayer est :' + activePlayer)
-  randomize();
-  console.log('function: ' + randomNumber)
+  randomize()
+  console.log('nombre aleatoire' + randomNumber)
   resultImage();
-    //animer();
-  //si je fait 1 je passe mon tour
-  DiceOne(activePlayer);
-    //push randomnumber dans round1
-  if (randomNumber >1){
-  round1.push(randomNumber)}
-  else{
-    console.log(randomNumber)
-  }
-  for (k = 0; k <5; k++);
-  round1;
-  totalRound1();
+  //si je fait un je passe mon tour
+  DiceOne();
+  totalRound();
+  //push de[1] dans round1
+  //round1.push(randomNumber++);
+  //round1;
+  //totalRound1();
   //affiche la somme du tableau round1 sur ma page
-  message1.innerHTML = resultRound1;
-  console.log('ajout des valeur dans round1:'+ round1);}
+  //message1.innerHTML = resultRound1;
+  console.log('ajout des valeur dans round1:'+ round1);}        //parcour de mon tableau round1
+       // for(m = 0; m< round1.length; m++);
 
 
 //****************************APPEL DE MES FONCTIONS POUR BOUTON HOLD1*****************   
@@ -173,7 +178,6 @@ recup1.onclick = function (){
       totalGlobal();
       console.log('resultGlobal1 à ce niveau: ' + resultGlobal1)
       switchPlayer();
-      removeIn() 
       //la valeur de global 1 est maintenant resultGlobal1
       messageglobal1.innerHTML = resultGlobal1;
       message1.innerHTML = '0';
@@ -184,53 +188,44 @@ recup1.onclick = function (){
 
 //****************************APPEL DE MES FONCTIONS POUR BOUTON ROLLDICE2*****************
 lancer2.onclick = function resultats(){
-    console.log('mon activplayer est :' + activePlayer);
   randomize();
   resultImage();
-    //animer()
-  //si je fait 1 je passe mon tour
-  DiceOne(activePlayer);
- 
-  //push randomnumberdans round2
-  if (randomNumber >1){
-  round2.push(randomNumber)}
-  else{
-    console.log(randomNumber);
-  }
-  for (l = 0; l <5; l++);
-  round2;
-  totalRound2();
-  message2.innerHTML = resultRound2;
+  //si je fait un je passe mon tour
+  DiceOne();
+  totalRound();
+  //push de[2] dans round2
+  //round2.push(randomNumber++);
+  //round2;
+  //totalRound2();
+  //affiche la somme du tableau round1 sur ma page
+  //message2.innerHTML = resultRound2;
   console.log('ajout des valeur dans round2:'+ round2);}
-  
-   
-
+       
+        //parcour de mon tableau round2
+        //for(n = 0; n< round2.length; n++);
 
 //****************************APPEL DE MES FONCTIONS POUR BOUTON HOLD1*****************   
 recup2.onclick = function (){
 //formule pour ajouter le resultRound2 au global2 à revoir
       totalGlobal();
-      switchPlayer();
-      removeIn() 
+      console.log('totalglobal2'+totalGlobal)
+      switchPlayer();  
       //la valeur de global2 est maintenant resultGlobal2
       messageglobal2.innerHTML = resultGlobal2;
       message2.innerHTML = '0';
       //vidange du tableau round2 de ses valeurs
       round2= [];
-      console.log('valeur de round2 apres reset : ' + round2); 
+      console.log('valeur de round2 apres reset : ' + round2)  
     };
 
 
 //*********************SI UN DES JOUEURS A UN GLOBAL SUPERIEUR A 100 ALORS IL GAGNE******/   
   function winer(){ 
-  if(resultGlobal1 => 100){
-    alert('Player1 gagne !!😄')}
-    else if (resultGlobal2 => 100) {
-      alert('Player2 gagne !!😄')
-    }
-      else{
-        console.log(global1, global2)
-      }};
+  if(resultGlobal1 > 100 || resultGlobal2 >100){
+    alert('tu as gagné !!😄');}
+    else{ 
+      console.log('on continue');
+    }};
 
 
 /***************************FUNCTION POUR MES IMAGES DE VALEUR de[1]************************** */
@@ -248,7 +243,9 @@ recup2.onclick = function (){
 //    }
 //  }
 //}
-function removeIn(){
+function 
+
+removeIn(){
   document.getElementById('myImages');
   $("#myImages").empty();
   
